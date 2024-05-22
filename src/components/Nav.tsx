9da62f89c3e894878562a7c67b27a7a877c56e55
rayhannr <rayhannr07@gmail.com>
2024-05-22 21:49:38 +0700

    feat: allow reset members and transactions and add link to github

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,13 @@
+import { Github } from './Icons/Github'
 import { ThemeToggler } from './ThemeToggler'
 
 export const Nav = () => {
   return (
-    <nav className="flex flex-row justify-center py-4 px-4 sm:px-0 sm:w-3/4 lg:w-5/6 sm:m-auto font-medium">
-      <div className="flex justify-end items-center w-full">
+    <nav className="sticky top-0 z-50 flex flex-row justify-center p-2 sm:px-0 sm:w-3/4 lg:w-5/6 sm:m-auto font-medium bg-background">
+      <div className="flex justify-end items-center w-full gap-2">
+        <a href="https://github.com/rayhannr/talangeen" target="_blank" rel="noreferrer">
+          <Github className="w-5 h-5" />
+        </a>
         <ThemeToggler />
       </div>
     </nav>
--- src/components/Result/InfoModalV1.tsx
+++ src/components/Result/InfoModalV1.tsx
@@ -17,6 +17,7 @@ export const InfoModalV1 = ({ isOpen, onOpenChange }: Props) => {
             <li>B nalangin A 2000</li>
             <li>B nalangin A sama D masing-masing 7500</li>
             <li>C nalangin B 3000</li>
+            <li>C nalangin A 4000</li>
           </ul>
           <p>
             Setelah lu klik tombol berikon kalkulator, bakal muncul tabel di bawah yang nampilin duit talangan yang harus
@@ -25,12 +26,16 @@ export const InfoModalV1 = ({ isOpen, onOpenChange }: Props) => {
           <ul className="list-disc list-inside">
             <li>A balikin duit B 9500</li>
             <li>B balikin duit C 3000</li>
-            <li>C balikin duit A 5000</li>
+            <li>C balikin duit A 1000</li>
             <li>D balikin duit B 7500</li>
           </ul>
           <p>
             B nalangin A 2 kali, 7500 sama 2000, tapi bukan berarti A balikinnya 2 kali juga. A cukup balikin duit ke B sekali
             sebesar 9500
+          </p>
+          <p>
+            A nalangin C 5000, habis itu C nalangin A 4000. Ntar A gak perlu balikin duit C 4000 dan C balikin duit A 5000
+            karena itu intinya sama aja kek C balikin duit A 1000
           </p>
         </ModalBody>
       </ModalContent>
