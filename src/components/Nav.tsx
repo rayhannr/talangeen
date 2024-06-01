2d1343720a3860dafc613989f26899701705d34b
rayhannr <rayhannr07@gmail.com>
2024-06-01 22:13:03 +0700

    fix: move theme toggler to the right

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import { ThemeToggler } from './ThemeToggler'
 export const Nav = () => {
   return (
     <nav className="sticky top-0 z-50 flex flex-row justify-center p-2 sm:px-0 sm:w-3/4 lg:w-5/6 sm:m-auto font-medium bg-background">
-      <div className="flex justify-between items-center w-full gap-2">
+      <div className="flex justify-end items-center w-full gap-2">
         <ThemeToggler />
         <Menu />
       </div>
